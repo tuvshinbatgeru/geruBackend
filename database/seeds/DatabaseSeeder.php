b74ede1882ea4d021f9a799d5b55165121863129
AkilLis <g.tuvshinbat@yahoo.com>
2017-03-07 01:01:07 +0800

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+    	DB::table('users')->insert([
+            'username' => 'tuvshoo',
+            'registry_no' => 'ИГ93062414',
+            'email' => 'gan.tuvshinbat@gmail.com',
+            'first_name' => 'Gansukh',
+            'last_name' => 'Tuvshinbat',
+            'password' => bcrypt('secret'),
+        ]);
+
+        //$this->call(UsersTableSeeder::class);
     }
 }
